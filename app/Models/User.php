ea7be46992d2ffe354cabfc2931ed361aa4055ca
oussamamalih <osmmlh816@gmail.com>
2026-09-07 10:17:23 +0100

    edit user.php file

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,17 +16,15 @@ class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+protected $fillable = [ 
+    'name',
+     'email',
+      'password',
+       'role',
         ];
+    #[Hidden(['password', 'remember_token'])]
+     protected function casts(): array 
+     { 
+        return [ 'email_verified_at' => 'datetime', 'password' => 'hashed', ];
     }
 }
