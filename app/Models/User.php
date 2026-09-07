72032972c7e83d32c663332de5b874868c0e2067
oussamamalih <osmmlh816@gmail.com>
2026-09-07 14:45:53 +0100

    update user.php file

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
@@ -10,21 +9,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'role'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
-protected $fillable = [ 
-    'name',
-     'email',
-      'password',
-       'role',
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
         ];
-    #[Hidden(['password', 'remember_token'])]
-     protected function casts(): array 
-     { 
-        return [ 'email_verified_at' => 'datetime', 'password' => 'hashed', ];
     }
 }
